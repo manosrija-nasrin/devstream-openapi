4473d1d8aeba7ebcbf004e65fdc8cccf00719ec0
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-01-23 01:47:49 +0530

    patch: added java cache and npm package version command

--- scripts/hash-node-deps.js
+++ scripts/hash-node-deps.js
@@ -1,17 +1,17 @@
 // "./package.json";
 const FILEPATH = process.argv[2];
-if(!FILEPATH) {
+if (!FILEPATH) {
   console.error("Error: File path argument is required.");
-  console.error("Usage: node ./scripts/hash-root-node-deps.js <path-to-package.json>")
+  console.error("Usage: node ./scripts/hash-root-node-deps.js <path-to-package.json>");
   process.exit(1);
 }
 
 const crypto = require("crypto");
 const fs = require("fs");
 const packageJSON = JSON.parse(fs.readFileSync(FILEPATH, "utf8"));
-const {dependencies = {}, devDependencies = {}} = packageJSON;
+const { dependencies = {}, devDependencies = {} } = packageJSON;
 const hash = crypto.createHash("sha256");
 hash.update(JSON.stringify(dependencies));
-hash.update(JSON.stringify(devDependencies));
+// hash.update(JSON.stringify(devDependencies));
 const digest = hash.digest("hex");
 console.log(digest);
