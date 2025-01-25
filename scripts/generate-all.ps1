a6370e90ece7d1bd154f1051b0132f5a7c011622
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-01-26 03:57:31 +0530

    fix: stops generate all script on error

--- scripts/generate-all.ps1
+++ scripts/generate-all.ps1
@@ -1,6 +1,41 @@
-Remove-Item -Path ./build -Recurse -Force
+# The following lines ensure that the script stops as soon 
+# as it encounters the first error while executing it.
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+
+function ThrowOnNativeFailure {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]$Message = "Native Failure"
+    )
+
+    if (-not $?) {
+        throw $Message
+    }
+}
+
+$BuildDir = './build'
+
+if (Test-Path -Path $BuildDir) {
+    Remove-Item -Path $BuildDir -Recurse -Force
+}
+else {
+    Write-Host "[Generate All]: ${BuildDir} Not Found"
+}
+
 npm run openapi:mint
+ThrowOnNativeFailure -Message "OpenAPI Mint Failed"
+
 npm run openapi:generate-client
+ThrowOnNativeFailure -Message "OpenAPI Client Generation Failed"
+
 npm run openapi:generate-server
+ThrowOnNativeFailure -Message "OpenAPI Server Generation Failed"
+
 npm run openapi:generate-swagger
+ThrowOnNativeFailure -Message "OpenAPI Swagger JSON Generation Failed"
+
 npm run openapi:client-docs
+ThrowOnNativeFailure -Message "OpenAPI Client Docs Generation Failed"
